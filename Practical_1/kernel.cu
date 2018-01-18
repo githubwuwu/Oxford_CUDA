80e4fe7b871534ea25e838c67ff420cf08ba820a
wu_sibet <wuwulive@163.com>
2018-01-18 23:08:49 +0800

    Practical_1_b 错误检查和Practical_1_c 统一内存的使用

--- Practical_1/kernel.cu
+++ Practical_1/kernel.cu
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-
+#include <helper_cuda.h>
+#include <helper_string.h>
+#include <cuda_runtime.h>
 //
 // kernel routine
 // 
@@ -28,30 +30,37 @@ int main(int argc, char **argv)
 	float *h_x, *d_x;
 	int   nblocks, nthreads, nsize, n;
 
+	// 初始化
+	int  devID= findCudaDevice(argc, (const char**)argv);
+	cudaDeviceProp deviceProps;
+	checkCudaErrors(cudaGetDeviceProperties(&deviceProps, devID));
+	printf("CUDA device [%s]\n", deviceProps.name);
+
 	// set number of blocks, and threads per block
 
-	nblocks = 2;
+	nblocks = 0;
 	nthreads = 8;
 	nsize = nblocks*nthreads;
 
 	// allocate memory for array
 
 	h_x = (float *)malloc(nsize*sizeof(float));
-	cudaMalloc(&d_x, nsize*sizeof(float));
+	checkCudaErrors(cudaMalloc(&d_x, nsize*sizeof(float)));
 
 	// execute kernel
 
 	my_first_kernel << <nblocks, nthreads >> >(d_x);
+	getLastCudaError("my_first_kernel execution failed\n");
 
 	// copy back results and print them out
 
-	cudaMemcpy(h_x, d_x, nsize*sizeof(float), cudaMemcpyDeviceToHost);
+	checkCudaErrors(cudaMemcpy(h_x, d_x, nsize*sizeof(float), cudaMemcpyDeviceToHost));
 
 	for (n = 0; n<nsize; n++) printf(" n,  x  =  %d  %f \n", n, h_x[n]);
 
 	// free memory 
 
-	cudaFree(d_x);
+	checkCudaErrors(cudaFree(d_x));
 	free(h_x);
 
 	// CUDA exit -- needed to flush printf write buffer
@@ -60,4 +69,8 @@ int main(int argc, char **argv)
 	cudaDeviceReset();
 
 	return 0;
+
+	//如果使用统一内存的话 只需要申请一次内存
+	//checkCudaErrors(cudaMallocManaged(&x, nsize*sizeof(float)));
+	// cudaDeviceSynchronize();
 }
